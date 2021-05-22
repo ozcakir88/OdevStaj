5a85c83dd278cca495d4748006625a6371315c2e
HP <HP@DESKTOP-5A4885V>
2021-05-22 17:31:56 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Add(Product product)
+        {
+           _productDal.Add(product);
+            
+        }
+
+        public void Delete(Product product)
+        {
+            _productDal.Delete(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public void Uptade(Product product)
+        {
+            _productDal.Update(product);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            
+            Product product1 = new Product();
+            
+           
+
+            productManager.GetAll();
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.Name);
+            }
+
+
+        }
+    }
+}
